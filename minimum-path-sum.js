93b7b911665aa8045014cbe56fb3c7532bf41682
Eleen Yeh <eleenyeh@gmail.com>
2020-11-07 21:17:39 -0800

    dp

--- minimum-path-sum.js
+++ minimum-path-sum.js
@@ -21,7 +21,23 @@ n == grid[i].length
 0 <= grid[i][j] <= 100
 */
 
-// brute force
+// BRUTE FORCE
+// input: m x n matrix
+// output: minimum sum of path from starting point to ending point
+// constraints:
+// edge: if matrix is null or length is 0, return 0
+
+// start [0,0]
+// end [m-1, n-1]
+
+// smallest sum variable = 0
+
+// dfs function(row, col, total)
+//     - base case: if we reach end coords, then we check the total with the smallest sum and reassign smallest sum to the smaller number
+//     - base case: if we reach outside of the matrix, return (stop recursion)
+//     - total += matrix[row][col]
+//     - recurse to the right
+//     - recurse down
 var minPathSum = function(grid) {
   if (!grid || grid.length === 0) return 0;
 
@@ -45,4 +61,27 @@ var minPathSum = function(grid) {
   dfs(0, 0, 0);
 
   return minSum;
+};
+
+// DP
+
+var minPathSum = function(grid) {
+  // find sum of top row
+  for (let i = 1; i < grid[0].length; i++) {
+    grid[0][i] += grid[0][i-1];
+  }
+
+  // find sum of left col
+  for (let j = 1; j < grid.length; j++) {
+    grid[j][0] += grid[j-1][0];
+  }
+
+  // find min sum of all other spaces in matrix
+  for (let r = 1; r < grid.length; r++) {
+    for (let c = 1; c < grid[0].length; c++) {
+      grid[r][c] = grid[r][c] + Math.min(grid[r-1][c], grid[r][c-1]);
+    }
+  }
+
+  return grid[grid.length-1][grid[0].length-1]
 };
